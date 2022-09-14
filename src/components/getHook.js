6c2af10d9707cc8621032f003b8876af30233b7a
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-14 20:32:49 +0300

    added get, post, and delete custom hooks

--- src/components/getHook.js
+++ src/components/getHook.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function useGetMethod(){
-    return
+function useGetMethod(url){
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    },[url])
+    return {
+        data,
+        setData
+    }
 }
+
+
+function usePostMethod(url, obj){
+    const[data, setData] = useState([])
+
+    fetch(url,{
+        method: "POST", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(obj)
+    })
+    .then(res=>res.json())
+    .then(data=> setData(data)) 
+
+    return {
+        data, setData
+    }
+    
+}
+
+function useDeleteMethod(url, id){
+    const [data, setData] = useState([])
+
+    fetch(url, {method: "DELETE"})
+    .then(res=>res.json())
+    .then(()=>{
+        const newData = data.filter(item=>item.id !== id)
+        setData(newData)
+    })
+}
+export {usePostMethod, useDeleteMethod}
 
 export default useGetMethod
